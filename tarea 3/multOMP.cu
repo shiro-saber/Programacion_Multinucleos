30b6ff184495be871532ec9246de422e1f85d529
shiro-saber <shiro.archerl@gmail.com>
2016-09-05 13:05:25 -0500

    OMP

--- tarea 3/multOMP.cu
+++ tarea 3/multOMP.cu
@@ -74,8 +74,7 @@ int main(int argc, char *argv[])
   cudaEventCreate( &fin );
   cudaEventRecord( inicio, 0 );
   //hacemos la multiplicacion
-  omp_set_num_threads(4);
-  #pragma omp parallel for private(k)
+  #pragma omp parallel for private(j,k)
     for(i=0; i<N; ++i)
       for(j=0; j<N; ++j)
         for(k=0; k<N; ++k)
@@ -86,7 +85,7 @@ int main(int argc, char *argv[])
   cudaEventElapsedTime( &tiempo, inicio, fin );
 
   //imprimimos el resultado
-  if(atoi(argv[1]) == 0)
+  if(atoi(argv[2]) == 0)
   {
     printf("\n\n\n");
     print_matrix(res,N);
