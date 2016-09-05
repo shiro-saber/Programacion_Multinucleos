532668e65d5cc5527d717b12d5847d59b1724964
shiro-saber <shiro.archerl@gmail.com>
2016-09-05 12:57:34 -0500

    hola

---
+++ Practicas/Practica1.cu
@@ -0,0 +1,18 @@
+/* primer practica Moi */
+#include <stdio.h>
+
+/* Definicion de bloques */
+#define N 1000
+
+__global__ void funcion()
+{
+
+}
+
+int main(void)
+{
+  
+
+  printf("Herr Moy\n");
+  return 0;
+}
