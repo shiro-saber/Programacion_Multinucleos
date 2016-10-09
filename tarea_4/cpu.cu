97c01f9c3f40c6d2f8c9e51b45a55f3769153003
shiro-saber <shiro.archerl@gmail.com>
2016-10-09 15:33:51 -0500

    Agregue crono al tuyo Jaime

--- tarea_4/cpu.cu
+++ tarea_4/cpu.cu
@@ -1,15 +1,13 @@
 #include<iostream>
+#include"stdio.h"
 #include<cmath>
 #include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/highgui/highgui.hpp>
  
- using namespace std;
- using namespace cv;
+using namespace std; //un comentario para algo que hacemos siempre
+using namespace cv; //si, este es para incluir el namespace cv de opencv
   
-   
-// Computes the x component of the gradient vector
-// at a given point in a image.
-// returns gradient in the x direction
+/* Saca el gradiente de x, en el punto dado en la direccion de x*/
 int xGradient(Mat image, int x, int y)
 {
   return image.at<uchar>(y-1, x-1) + 
@@ -17,12 +15,10 @@ int xGradient(Mat image, int x, int y)
          image.at<uchar>(y+1, x-1) - 
          image.at<uchar>(y-1, x+1) - 
          2*image.at<uchar>(y, x+1) - 
-         image.at<uchar>(y+1, x+1);
+         image.at<uchar>(y+1, x+1); // return gigantezco de las operaciones que se ejecutan
 }
     
-// Computes the y component of the gradient vector
-// at a given point in a image
-// returns gradient in the y direction
+/* lo mismo del de arriba pero ahora cambia las x por y */
 int yGradient(Mat image, int x, int y)
 {
   return image.at<uchar>(y-1, x-1) +
@@ -30,43 +26,63 @@ int yGradient(Mat image, int x, int y)
          image.at<uchar>(y-1, x+1) -
          image.at<uchar>(y+1, x-1) -
          2*image.at<uchar>(y+1, x) -
-         image.at<uchar>(y+1, x+1);
+         image.at<uchar>(y+1, x+1);// mismo return gigantezco que se veia feo en una linea
 }
       
-int main()
+int main() //¿qué será esto?
 {
-  Mat src, src2, dst;
-  int gx, gy, sum;
+  Mat src, src2, dst; //pos las imágenes
+  int gx, gy, sum; // dejare un comentario en esta declaracion
+  cudaEvent_t inicio, fin,st2,fn2; // cida events \o/
+  float tiempo, tiempo2; // dios mio, ¿que será?
                       
-  // Load an image
-  src = imread("cancer.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-  src2 = imread("cancer.jpg", CV_LOAD_IMAGE_COLOR);
-  dst = src.clone();
+  // Carguemos la imagen
+  src = imread("salon.jpeg", CV_LOAD_IMAGE_GRAYSCALE); // es más fácil hacerlo asi
+  src2 = imread("salon.jpeg", CV_LOAD_IMAGE_COLOR); // pos necesitas mostrarla en algún momento no?
+  dst = src.clone(); //hagamos un clon
   
-  if( !src.data ) return -1;
+  cudaEventCreate( &st2 ); //mummy
+  cudaEventCreate( &fn2 ); //dracula
+  cudaEventRecord( st2, 0 ); //frankie
+
+  if( !src.data ) return -1; // no manches, pasame algo
                                                                                                         
-  for(int y = 0; y < src.rows; y++)
-    for(int x = 0; x < src.cols; x++)
-      dst.at<uchar>(y,x) = 0.0;
-                                                                                                 
+  for(int y = 0; y < src.rows; y++) //recorramos las filas
+    for(int x = 0; x < src.cols; x++) //recorramos las columnas
+      dst.at<uchar>(y,x) = 0.0; //punto inicial
+  
+  cudaEventCreate( &inicio );// esta vivo!!!
+  cudaEventCreate( &fin ); // el bebe esta vivo!!!
+  cudaEventRecord( inicio, 0 );//para medir el tiempo de EJECUCION
+
   for(int y = 1; y < src.rows - 1; y++){
     for(int x = 1; x < src.cols - 1; x++){
-      gx = xGradient(src, x, y);
-      gy = yGradient(src, x, y);
-      sum = abs(gx) + abs(gy);
-      sum = sum > 255 ? 255:sum;
-      sum = sum < 0 ? 0 : sum;
-      dst.at<uchar>(y,x) = sum;
+      gx = xGradient(src, x, y); //dame el gradiente x
+      gy = yGradient(src, x, y); //dame el gradiente y
+      sum = abs(gx) + abs(gy); //suma que pues tenía que hacer
+      sum = sum > 255 ? 255:sum; //ahora si la suma es mayor a 255 cierralo en 255 si no pues dejalo en paz
+      sum = sum < 0 ? 0 : sum; // y que tal si es menor a 0 pos no te vas a los negativos
+      dst.at<uchar>(y,x) = sum; // y vamos pasando los puntos.
     }
   }
-                                                                                                                                                        
-  namedWindow("final");
-  imshow("final", dst);
-                                                                                                                    
-  namedWindow("initial");
-  imshow("initial", src2);
+  cudaEventRecord( fin, 0 ); // esta muerto ¬¬
+  cudaEventSynchronize( fin ); //sincronizadas
+  cudaEventElapsedTime( &tiempo, inicio, fin );//paramos cronometro y medimos tiempo
 
-  waitKey();
+                                                                                                                                                        
+  namedWindow("final"); // hagamos un muñeco, digo una ventana
+  imshow("final", dst); // print a la imagen generada 
+                                                                                                                    
+  namedWindow("initial"); //ya me puedo ir? 
+  imshow("initial", src2); //neta lo tengo que comentar otra vez?
+  
+  cudaEventRecord( fn2, 0 ); //dont kill me, please!
+  cudaEventSynchronize( fn2 ); //free the nipple
+  cudaEventElapsedTime( &tiempo2, st2, fn2 );//paramos cronometro y medimos tiempo
+  
+  printf("tiempo calculos en ms: %f\t tiempo de total %f\n", tiempo,tiempo2);//cazar elefantes... o imprimir no se solo soy un comentario
+
+  waitKey(); //no se si esto es demasiado obvio para comentarlo
           
-  return 0;
+  return 0; // y yo? que hago aqui?
 }
--- tarea_4/main.cu
+++ tarea_4/main.cu
@@ -8,11 +8,18 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    cudaEvent_t inicio, fin,st2,fn2;
+    float tiempo, tt2;
+    
+    cudaEventCreate( &st2 );
+    cudaEventCreate( &fn2 );
+    cudaEventRecord( st2, 0 );
+
     IplImage* image;
     IplImage* sharmuta;
 
-    image = cvLoadImage("cancer.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    sharmuta = cvLoadImage("cancer.jpg", CV_LOAD_IMAGE_COLOR);
+    image = cvLoadImage("salon.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    sharmuta = cvLoadImage("salon.jpeg", CV_LOAD_IMAGE_COLOR);
 
     if(!image )
     {
@@ -29,19 +36,33 @@ int main(int argc, char** argv)
 
     float *output = (float*)image2->imageData;
     float *input =  (float*)image3->imageData;
+    
+    cudaEventCreate( &inicio );
+    cudaEventCreate( &fin );
+    cudaEventRecord( inicio, 0 );//para medir el tiempo de EJECUCION
 
     kernelcall(input, output, image->width,image->height, image3->widthStep);
+    
+    cudaEventRecord( fin, 0 );
+    cudaEventSynchronize( fin );
+    cudaEventElapsedTime( &tiempo, inicio, fin );//paramos cronometro y medimos tiempo
 
     //Normalize the output values from 0.0 to 1.0
     cvScale(image2,image2,1.0/255.0);
 
     cvShowImage("Original Image", sharmuta );
     cvShowImage("Sobeled Image", image2);
+    
+    cudaEventRecord( fn2, 0 );
+    cudaEventSynchronize( fn2 );
+    cudaEventElapsedTime( &tt2, st2, fn2 );//paramos cronometro y medimos tiempo
+    printf("tiempo calculos en ms: %f\t tiempo de total %f\n", tiempo,tt2);//cazar elefantes... o imprimir no se solo soy un comentario
+
+    return 0;
+}
+
+
     cvWaitKey(0);
-    // namedWindow("Input",WINDOW_NORMAL);
-    // namedWindow("Output",WINDOW_NORMAL);
-    // imshow("Input",image);
-    // imshow("Output",image2);
-    // waitKey();
+    
     return 0;
 }
